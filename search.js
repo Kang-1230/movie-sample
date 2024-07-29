9f912d138b779dd405d555a2600607dc8b786d37
unknown <tnwls7138@gmail.com>
2024-07-29 12:58:37 +0900

    필수 기능 구현 완료

--- search.js
+++ search.js
@@ -1,12 +1,20 @@
 document.getElementById("search-button").addEventListener("click", () => {
   const query = document.getElementById("search-input").value.toLowerCase();
+  console.log("검색어:", query);
   const movieCards = document.querySelectorAll(".movie-card");
+  console.log("영화 카드 수:", movieCards.length);
   movieCards.forEach((card) => {
-    const title = card.querySeleter("h3").textContent.toLowerCase();
-    if (title.includes(query)) {
-      card.style.display = "block";
+    const titleElement = card.querySelector("h3");
+    if (titleElement) {
+      const title = titleElement.textContent.toLowerCase();
+      console.log("영화 제목:", title);
+      if (title.includes(query)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
     } else {
-      card.style.display = "none";
+      console.error("영화 카드에 <h3> 요소가 없습니다.");
     }
   });
 });
